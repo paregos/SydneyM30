8afa4d996b4ac61827eaab82386f95a73955d269
Ben Mitchell <ben.mitchell@rokt.com>
2018-04-08 14:03:58 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# GOING HOME HELLA FAST 
+
+#How to install
+
+- Install node.js (with chocolately and powershell on windows)
+- Clone repo
+- cd into repo
+- "npm install --save"
+
+
+#How to run
+
+- cd into repo
+- node server.js
+- navigate to localhost:3010
---
+++ server.js
@@ -0,0 +1,35 @@
+const express=require('express');
+
+// Initialisation
+const app=express();
+const router = express.Router();
+
+app.use(express.static(__dirname + '/src'));
+
+app.set('views',__dirname);
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+app.get('*', (req, res) => {
+  res.render('./src/index.html');
+});
+
+// Error handling
+app.use(function (err, req, res, next) {
+    // Return 500 for any uncaught errors
+    console.log(err);
+    res.status(500).send('Something broke!');
+});
+
+// cron.schedule('0 0 */1 * * *', function () {
+//     console.log('running a task every 5 seconds');
+//     recommendedlisting.calculateRecommendations();
+// });
+
+var server=app.listen((process.env.PORT || 3010),function(){
+console.log("We have started our server on port " +(process.env.PORT || 3010));
+});
+
+
+module.exports = app;
